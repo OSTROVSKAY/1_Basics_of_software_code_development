19edb731f4889791039513a1215f0746c6d988c9
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-04-20 12:30:54 +0300

    1

---
+++ src/Task_1_2/Task_1_2.java
@@ -0,0 +1,47 @@
+
+/* 2. Вычислить значение выражения по формуле (все переменные принимают действительные значения):
+      Функция содержит вычисление квадратного корня, рациональную дробь, и отрицательную степень числа */
+
+package Task_1_2;
+
+import java.util.Scanner;
+
+public class Task_1_2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("ПРОГРАММА РАСЧЁТА ЗНАЧЕНИЯ ФУНКЦИИ");
+		System.out.println("----------------------------------");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		double z;
+		double a;
+		double b;
+		double c;
+		
+		System.out.println("Введите значения : a, b, c");
+		System.out.println("----------------------------------");
+		
+		System.out.print("Введите значения : a...........:");
+		a = scanner.nextDouble();
+		System.out.println("----------------------------------");
+		
+		System.out.print("Введите значения : b...........:");
+		b = scanner.nextDouble();
+		System.out.println("----------------------------------");
+		
+		System.out.print("Введите значения : c...........:");
+		c = scanner.nextDouble();
+		System.out.println("----------------------------------");
+		
+		z =
+		
+		System.out.println("----------------------------------");
+		System.out.println("ЗНАЧЕНИЕ ФУНКЦИИ РАВНО :" + "   " + z);
+		System.out.println("----------------------------------");
+		
+		scanner.close();
+	}
+
+}
