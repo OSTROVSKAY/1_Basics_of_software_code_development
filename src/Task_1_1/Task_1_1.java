4640375d2c4cdcc12dba8a3fadaa47ec883b0d14
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-04-20 11:32:36 +0300

    1

---
+++ src/Task_1_1/Task_1_1.java
@@ -0,0 +1,13 @@
+package Task_1_1;
+
+// 1. Найдите значение функции: z = ( (a – 3 ) * b / 2) + c //
+
+
+public class Task_1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
