ce7b2ae3e4d3ae51d159dcd57f1fe4e15a1e15fe
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-04-27 23:15:49 +0300

    1

---
+++ src/Task_3_8/Task_3_8_res.java
@@ -0,0 +1,50 @@
+package Task_3_8;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Task_3_8_res {
+
+	public int InputInt() {
+		  
+	      int check = 1;
+	      int x  = 0;
+	     
+	      do {
+		       Scanner scanner = new Scanner(System.in);
+		       System.out.print("Введите Данные :.............:" + "  ");
+               String xx = scanner.nextLine();
+   
+               check = 1;
+       
+                    try {
+                  	      x = Integer.parseInt(xx);
+                        } 
+                          catch (NumberFormatException e) {
+                          
+           	            check = 0;
+       	                System.out.println("------------------------------------------");
+       	                System.out.println("Данные введены не правильно. Введите снова");
+       	                System.out.println("------------------------------------------");
+                        }
+                    
+              System.out.println("------------------------------");
+   
+       } while ( check == 0 );
+		    return x; 
+   }
+	
+	
+	
+	public  ArrayList numberEnter(int a){
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        while(a > 0){
+            int i = 0;
+            array.add(i, a % 10);
+            a = a / 10;
+            i++;
+        }
+        return array;
+    }
+
+}
