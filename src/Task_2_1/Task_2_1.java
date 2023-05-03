f44dfc87e4a1fb4fb9ee6c621f2d456e1b0fc00d
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-05-03 22:17:32 +0300

    1

--- src/Task_2_1/Task_2_1.java
+++ src/Task_2_1/Task_2_1.java
@@ -7,73 +7,75 @@
 
 package Task_2_1;
 
-import java.util.Scanner;
-
 public class Task_2_1 {
 
 	public static void main(String[] args) {
 		
 		System.out.println("ПРОГРАММА ОПРЕДЕЛЕНИЯ ВИДА ТРЕУГОЛЬНИКА");
-		System.out.println("----------------------------------");
-		
-        Scanner scanner = new Scanner(System.in);
+		System.out.println("---------------------------------------");
 		
 		double A;
 		double B;
 		
-		do {
+		Task_2_1_res Inp = new Task_2_1_res();
 		
+		System.out.println("-----------------------------------------------------");
 		System.out.println("Введите значения углов треугольников в градусов: A, B");
-		System.out.println("----------------------------------");
+		System.out.println("-----------------------------------------------------");
 		
-		System.out.print("Введите значения угла A...........:");
-		A = scanner.nextDouble();
-		System.out.println("----------------------------------");
+		do {
+		    System.out.println("Введите значения угла A...........:");
+		    System.out.println("-----------------------------------");
+		    A = Inp.Input();
+		   
+		           if ( A<=0 )
+			          {
+		        	   System.out.println("-----------------------------------------");
+				       System.out.println("Введите значения угла треугольника заново");
+				       System.out.println("-----------------------------------------");
+			          }
+		   } while ( A<=0 );
 		
-		System.out.print("Введите значения угла B...........:");
-		B = scanner.nextDouble();
-		System.out.println("----------------------------------");
 		
-		System.out.println("----------------------------------");
-		
-		  if ( (A<=0) | (B<=0) )
-		  {
-			  System.out.println("Введите значения углов треугольника: A и B заново");
-			  System.out.println("----------------------------------");
-			  System.out.println("----------------------------------");
-		  }
-		
-		} while ( (A<=0) | (B<=0) );
-		
-	
-		if( (A+B)<180) // Определение существования треугольника
-		{
-			System.out.println("Треугольник существует");
-			System.out.println("----------------------------------");
-			System.out.println("----------------------------------");
+		do {
+		    System.out.println("Введите значения угла B...........:");
+		    System.out.println("-----------------------------------");
+		    B = Inp.Input();
+		   
+		           if ( B<=0 )
+			          {
+		        	   System.out.println("-----------------------------------------");
+				       System.out.println("Введите значения угла треугольника заново");
+				       System.out.println("-----------------------------------------");
+			          }
 			
+			} while ( B<=0 );
+		  
+
+		if( (A+B) < 180 ) // Определение существования треугольника
+		     { 
+			   System.out.println("----------------------");
+			   System.out.println("Треугольник существует");
+			   System.out.println("----------------------");
 			
-			 if( 180-(A+B)==90 ) // Определение вида треугольника
-			 {
-				 System.out.println("Треугольник прямоугольный");
-				 System.out.println("----------------------------------");
-				 System.out.println("----------------------------------");
-				 
-			 }
-			
-			 else {
-				    System.out.println("Треугольник не прямоугольный");
-				    System.out.println("----------------------------------");
-				    System.out.println("----------------------------------");
+			   if( (180-(A+B)) == 90 ) // Определение вида треугольника. Расчёт третьего угла треугольника : Угол C.
+			       {
+				    System.out.println("-------------------------");
+				    System.out.println("Треугольник прямоугольный");
+				    System.out.println("-------------------------");
 			       }
-		}
-		else
-		    {
-			System.out.println("Треугольник не существует");
-			System.out.println("----------------------------------");
-			System.out.println("----------------------------------");
+			
+			       else {
+				         System.out.println("----------------------------");
+				         System.out.println("Треугольник не прямоугольный");
+				         System.out.println("----------------------------");
+			            }
 		    }
-		
-		scanner.close();
+		     else
+		         {
+			      System.out.println("-------------------------");
+			      System.out.println("Треугольник не существует");
+			      System.out.println("-------------------------");
+		         }
 	}
 }
---
+++ src/Task_2_1/Task_2_1_res.java
@@ -0,0 +1,36 @@
+
+package Task_2_1;
+
+import java.util.Scanner;
+
+public class Task_2_1_res {
+	
+	// Ввод данных
+	
+	 public double Input () {
+		  
+	      int check = 1;
+	      double x  = 0;
+	     
+	      do {
+	    	    Scanner scanner = new Scanner(System.in);
+		        System.out.print("Введите Данные :.............:" + "  ");
+                String xx = scanner.nextLine();
+               
+               check = 1;
+       
+                        try {
+                              x = Double.parseDouble(xx);
+                            } 
+                              catch (NumberFormatException e) {
+               	            check = 0;
+           	                System.out.println("------------------------------------------");
+           	                System.out.println("Данные введены не правильно. Введите снова");
+           	                System.out.println("------------------------------------------");
+                            }
+       System.out.println("----------------------------------");
+       
+      } while ( check == 0 );
+		return x; 
+  }
+}
