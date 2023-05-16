82d8eade4ce7a8719c74d42577bcb911b56c7986
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-05-16 18:17:58 +0300

    1

--- src/Task_2_5/Task_2_5.java
+++ src/Task_2_5/Task_2_5.java
@@ -1,14 +1,14 @@
-
+	
 /*
-   5. Вычислить значение функции.
+	5. Вычислить значение функции.
 */
 
 package Task_2_5;
-
+	
 import java.util.Scanner;
-
+	
 public class Task_2_5 {
-
+	
 	public static void main(String[] args) {
 		
 		System.out.println("ПРОГРАММА РАСЧЁТА ЗНАЧЕНИЯ ФУНКЦИИ");
@@ -25,42 +25,39 @@ public class Task_2_5 {
 		System.out.println("Введите значение : x");
 		System.out.println("--------------------");
 		
-		 do {
-			    System.out.println("---------------------------------");
-	            System.out.print("Введите значение : x............: " + "  ");
-	            String xx = scanner.nextLine();
-	            
-	            check = 1;
-	            
-	            try {
-	                  x = Double.parseDouble(xx);
-	                     } catch (NumberFormatException e) {
-	                    	                                 check = 0;
-	                	                                     System.out.println("-------------------------------------------");
-	                	                                     System.out.println("Данные введены не правильно. Введите снова");
-	                	                                     System.out.println("-------------------------------------------");
-	                                                       }
-	           }
-	        	
-	     while (check == 0); 
-		 
-		                     // Вычисление значение функции :
-		 
-		 if ( x > 3  &  ( Math.pow(x,3) + 6 ) == 0 ) {
-			  System.out.println("---------------------------------");
-		      System.out.println("ЗНАЧЕНИЕ ФУНКЦИИ НЕЛЬЗЯ ВЫЧИСЛИТЬ");
-			  System.out.println("---------------------------------");
-		 }
-		 
-		 if ( x > 3  &  ( Math.pow(x,3) + 6 ) != 0 )
-		      { 
-			    y = 1 / ( Math.pow(x,3) + 6 );
-		      }
-		         else
-		             {
-			           y = Math.pow(x,2) - 3*x + 9;
-		             }
-		 
+		do {
+			System.out.println("---------------------------------");
+			System.out.print("Введите значение : x............: " + "  ");
+			String xx = scanner.nextLine();
+			
+			check = 1;
+			
+				try {
+					x = Double.parseDouble(xx);
+					} catch (NumberFormatException e) {
+					check = 0;
+						System.out.println("-------------------------------------------");
+						System.out.println("Данные введены не правильно. Введите снова");
+						System.out.println("-------------------------------------------");
+					}
+				}while (check == 0);
+									// Вычисление значение функции :
+			
+		if ( x > 3  &  ( Math.pow(x,3) + 6 ) == 0 ) {
+				System.out.println("---------------------------------");
+				System.out.println("ЗНАЧЕНИЕ ФУНКЦИИ НЕЛЬЗЯ ВЫЧИСЛИТЬ");
+				System.out.println("---------------------------------");
+		}
+		
+		if ( x > 3  &  ( Math.pow(x,3) + 6 ) != 0 )
+				{
+				y = 1 / ( Math.pow(x,3) + 6 );
+				}
+				else
+					{
+					y = Math.pow(x,2) - 3*x + 9;
+					}
+		
 		System.out.println("-------------------------------------------------");
 		System.out.println("-------------------------------------------------");
 		System.out.println("ЗНАЧЕНИЕ ФУНКЦИИ РАВНО : " + " " + y);
@@ -69,5 +66,5 @@ public class Task_2_5 {
 		
 		scanner.close();
 	}
-
+	
 }
