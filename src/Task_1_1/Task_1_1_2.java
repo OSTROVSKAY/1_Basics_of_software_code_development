2274507c09e78228043d6c2e9a2ccc02b979a5e7
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-05-16 10:21:42 +0300

    1

--- src/Task_1_1/Task_1_1_2.java
+++ src/Task_1_1/Task_1_1_2.java
@@ -1,14 +1,14 @@
-                      
-                            // КОНСОЛЬНОЕ ПРИЛОЖЕНИЕ ДЛЯ JAVA //
-                    // 1. Найдите значение функции: z = ( (a – 3 ) * b / 2) + c //
-
+								
+								// КОНСОЛЬНОЕ ПРИЛОЖЕНИЕ ДЛЯ JAVA //
+							// 1. Найдите значение функции: z = ( (a – 3 ) * b / 2) + c //
+							
 package Task_1_1;
-
+	
 import java.io.Console;
 import java.io.IOException;
-
+	
 public class Task_1_1_2 {
-
+	
 	public static void main(String[] args) throws IOException {
 		
 		System.out.println("ПРОГРАММА РАСЧЁТА ЗНАЧЕНИЯ ФУНКЦИИ");
@@ -29,42 +29,42 @@ public class Task_1_1_2 {
 		System.out.println("----------------------------------");
 		
 		// Получение консоли
-        Console console = System.console();
-        if(console!=null){
-            // Считывание данных с консоли
-            // String a1 = console.readLine("Введите значение : a...........:");
-            // a = Double.parseDouble(a1);
-        	a = Inp.Input();
-         }
-		System.out.println("----------------------------------");
-		
-		
-		    System.out.println("Введите значение b : ");
-		    System.out.println("----------------------------------");
-		 if(console!=null){
-	            // Считывание данных с консоли
-	            // String b1 = console.readLine("Введите значение : b...........:");
-	            // b = Double.parseDouble(b1);
-			    b = Inp.Input();
-	        }
-		System.out.println("----------------------------------");
-		
-		
-		    System.out.println("Введите значение c : ");
-		    System.out.println("----------------------------------");
+		Console console = System.console();
 		if(console!=null){
-              // Считывание данных с консоли
-              // String c1 = console.readLine("Введите значение : c...........:");
-			  // c = Double.parseDouble(c1);
-              c = Inp.Input();
-        }
-		 
+			// Считывание данных с консоли
+			// String a1 = console.readLine("Введите значение : a...........:");
+			// a = Double.parseDouble(a1);
+			a = Inp.Input();
+		}
 		System.out.println("----------------------------------");
 		
-		                 // 1. Найдите значение функции: z = ( (a – 3 ) * b / 2) + c //
 		
-		       z = ( (a-3) * b/2 ) + c;
+			System.out.println("Введите значение b : ");
+			System.out.println("----------------------------------");
+		if(console!=null){
+				// Считывание данных с консоли
+				// String b1 = console.readLine("Введите значение : b...........:");
+				// b = Double.parseDouble(b1);
+				b = Inp.Input();
+			}
+		System.out.println("----------------------------------");
+			
+			
+			System.out.println("Введите значение c : ");
+			System.out.println("----------------------------------");
+		if(console!=null){
+				// Считывание данных с консоли
+				// String c1 = console.readLine("Введите значение : c...........:");
+				// c = Double.parseDouble(c1);
+				c = Inp.Input();
+		}
 		
+		System.out.println("----------------------------------");
+		
+					// 1. Найдите значение функции: z = ( (a – 3 ) * b / 2) + c //
+			
+					z = ( (a-3) * b/2 ) + c;
+					
 		System.out.println("--------------------------------------------");
 		System.out.println("ЗНАЧЕНИЕ ФУНКЦИИ РАВНО :" + "   " + z);
 		System.out.println("--------------------------------------------");
